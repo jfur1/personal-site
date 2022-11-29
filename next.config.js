e84c651baba239c8aa486f26de411e26f1702bdc
John Furlong <johnfurlong24@gmail.com>
2022-11-28 17:09:36 -0700

    temp remove toggle

--- next.config.js
+++ next.config.js
@@ -5,8 +5,11 @@ const nextConfig = {
     unoptimized: true
   },
   // NODE_ENV gets set based on the script -- see package.json
-  basePath: process.env.NODE_ENV === "production" ? "/personal-site" : undefined,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/personal-site" : undefined,
+  // basePath: process.env.NODE_ENV === "production" ? "/personal-site" : undefined,
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/personal-site" : undefined,
+
+  basePath: "/personal-site",
+  assetPrefix: "/personal-site"
 }
 
 module.exports = nextConfig
